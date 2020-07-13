ebd0bd134912ec5a75ed410833a88cbca1afb944
connor-myers <ConnorJamesMyers@protonmail.com>
2020-07-13 20:30:38 +1000

    changed overengineered stack implementation to make it more applicable

--- main.c
+++ main.c
@@ -6,27 +6,5 @@ int main(int argc, char **argv)
         err_msg(BAD_ARGS);
     }
 
-    Stack stack;
-    stack_init(&stack, sizeof(short));
-
-    short one = 1;
-    short two = 69;
-    short three = 3;
-
-    stack_push(&stack, (void *) &one);
-    stack_push(&stack, (void *) &two);
-    stack_push(&stack, (void *) &three);
-
-    short *first;
-    short *second;
-    short *third;
-
-    first = (short*) stack_pop(&stack); 
-    second = (short*) stack_pop(&stack); 
-    third = (short*) stack_pop(&stack); 
-
-    printf("%d, %d, %d\n", *first, *second, *third);
-
-
     return 0;
 }
--- stack.h
+++ stack.h
@@ -3,18 +3,16 @@
 
 #include <stdlib.h>
 
-#define INITIAL_CAPACITY 10     // the initial capacity of a stack
-#define GROWTH_RATE 2           // how much the stack grows by when it reaches capacity
+#define STACK_CAPACITY 16
 
 typedef struct Stack {
+    size_t capacity;            // the max size of the stack
     size_t size;                // the number of entries in the stack
-    size_t capacity;            // the max number of entries that can fit in this stack
-    size_t entrySize;           // the size of a single entry in the stack
-    void **entries;             // the entries in the stack
+    unsigned short *entries;    // the entires in the stack. Chip-8 stack uses 2 byte Program Counters, hence short
 } Stack;
 
-void stack_init(Stack *stack, size_t entrySize);
-void stack_push(Stack *stack, void *entry);
-void *stack_pop(Stack *stack);
+void stack_init(Stack *stack);
+void stack_push(Stack *stack, unsigned short entry);
+unsigned short stack_pop(Stack *stack);
 
 #endif
